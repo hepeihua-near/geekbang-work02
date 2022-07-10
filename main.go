0d9d956d421bb3f40c083212fdc90da47dd8fac4
hepeihua.566 <hepeihua.566@bytedance.com>
2022-07-11 00:27:30 +0800

    week02

--- main.go
+++ main.go
@@ -2,12 +2,15 @@ package main
 
 import (
 	"fmt"
+	"geekbang/week01/my_error"
 	"geekbang/week01/service"
+	"github.com/pkg/errors"
 )
 
 func main() {
 	student, err := service.NewService1().GetStudentById(1)
 	if err != nil {
+		fmt.Println(errors.Is(err, my_error.DateNotExist))
 		fmt.Printf("%+v", err)
 		return
 	}
